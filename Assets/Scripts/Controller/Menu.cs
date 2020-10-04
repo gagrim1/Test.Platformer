b0b205bad214fdfb3c8f75f80f2113b473749a28
A_small_herO <bhimyla224@gmail.com>
2020-10-04 18:50:58 +0300

    Camera&Menu
    Создал скрипт CameraController который делает так чтобы камера следила за игроком. Так же сделал скрипт Menu которое отвечает за функции 3-х кнопок в меню

---
+++ Assets/Scripts/Controller/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject MenuUI;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        MenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        MenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Debug.Log("Load");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
