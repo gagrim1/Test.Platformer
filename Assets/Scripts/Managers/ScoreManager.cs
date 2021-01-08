16c768e3b363583d54d9dd6daec2af1b5ea3a5ea
Mikhail Oleynik <oleinik.ms@phystech.edu>
2021-01-08 20:54:21 +0300

    feat coin and coin logic

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScoreManager : MonoBehaviour
+{
+    public PlayerData playerData;
+    UnityEvent scoreChangedEvent;
+    UIManager _ui;
+
+    void Start()
+    {
+        _ui = GameObject.FindWithTag("LevelScene").GetComponent<UIManager>();
+        playerData.coinScore = 0;
+        if (scoreChangedEvent == null) scoreChangedEvent = new UnityEvent();
+        scoreChangedEvent.AddListener(_ui.RedrawCoinScore);
+    }
+
+    public void GetCoin()
+    {
+        playerData.coinScore++;
+        scoreChangedEvent.Invoke();
+    }
+}
