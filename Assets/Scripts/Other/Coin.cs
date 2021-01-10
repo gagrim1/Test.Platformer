64871a29e541636053dd004cf93d06e7a1ee59e3
edjack <azarovdima2000@gmail.com>
2021-01-10 18:37:10 +0300

    feat moving platform not working

--- Assets/Scripts/Other/Coin.cs
+++ Assets/Scripts/Other/Coin.cs
@@ -6,7 +6,10 @@ public class Coin : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
-        col.gameObject.GetComponent<ScoreManager>().GetCoin();
-        Destroy(gameObject);
+        if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            col.gameObject.GetComponent<ScoreManager>().GetCoin();
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Other/MovingPlatform.cs
+++ Assets/Scripts/Other/MovingPlatform.cs
@@ -5,41 +5,59 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     public GameObject endObj;
+    public GameObject startObj;
     private Vector2 end;
     private Vector2 start;
     private Vector2 now;
+    private Vector2 move;
+    private Rigidbody2D rigidBody;
     public float speed;
     public bool direction=true;
+    public List<Rigidbody2D> passengers;
+
     private void Start()
     {
-        start = transform.position;
-        end = endObj.transform.position;
-        Destroy(endObj);
+        rigidBody = GetComponentInChildren<Rigidbody2D>();
+        transform.position = startObj.transform.position;
+        //Destroy(endObj);
     }
-
-    void Update()
+    private void FixedUpdate()
     {
+        start = startObj.transform.position;
+        end = endObj.transform.position;
         now = transform.position;
-        if (direction) 
+        if (direction)
         {
-            if (Vector2.Distance(now, end) <= speed * Time.deltaTime)
+            if (Vector2.Distance(now, end) <= speed * Time.fixedDeltaTime)
                 direction = false;
-            transform.position = now + (end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.deltaTime, 0f, Vector2.Distance(now, end));
+            move = (end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.fixedDeltaTime, 0f, Vector2.Distance(now, end));
+            //rigidBody.MovePosition(Vector2.Lerp(start, end, interpolate));
+            //transform.position = now + (end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.fixedDeltaTime, 0f, Vector2.Distance(now, end));
         }
         else
         {
-            if (Vector2.Distance(now, start) <= speed * Time.deltaTime)
+            if (Vector2.Distance(now, start) <= speed * Time.fixedDeltaTime)
                 direction = true;
-            transform.position = now - (end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.deltaTime, 0f, Vector2.Distance(now, start));
-        }   
+            move = -(end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.fixedDeltaTime, 0f, Vector2.Distance(now, start));
+            //rigidBody.MovePosition(Vector2.Lerp(end, start, interpolate));
+            //transform.position = now - (end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.fixedDeltaTime, 0f, Vector2.Distance(now, start));
+        }
+        rigidBody.MovePosition(now + move);
+        passengers.ForEach(Move);
+    }
+    private void Move(Rigidbody2D passenger)
+    {
+        passenger.transform.position=(Vector2)passenger.transform.position + move;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.transform.parent = transform;
+        passengers.Add(collision.gameObject.GetComponent<Rigidbody2D>());
+        //collision.gameObject.transform.parent = transform;
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        collision.gameObject.transform.parent = null;
+        passengers.Remove(collision.gameObject.GetComponent<Rigidbody2D>());
+        //collision.gameObject.transform.parent = null;
     }
 }
