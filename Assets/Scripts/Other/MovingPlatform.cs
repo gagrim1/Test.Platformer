d8e46742dbdfd28f564de2ebaad22bc8475b8edc
Mikhail Oleynik <oleinik.ms@phystech.edu>
2021-01-13 11:27:26 +0300

    fix MovingPlatform's movement

--- Assets/Scripts/Other/MovingPlatform.cs
+++ Assets/Scripts/Other/MovingPlatform.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public GameObject endObj;
-    public GameObject startObj;
-    private Vector2 end;
+    private RectTransform bounds;
+    public bool isHorizontal;
+    private Vector2 platformSize;
     private Vector2 start;
+    private Vector2 end; 
     private Vector2 now;
     private Vector2 move;
     private Rigidbody2D rigidBody;
@@ -17,47 +18,58 @@ public class MovingPlatform : MonoBehaviour
 
     private void Start()
     {
+        platformSize = gameObject.GetComponent<SpriteRenderer>().size;
         rigidBody = GetComponentInChildren<Rigidbody2D>();
-        transform.position = startObj.transform.position;
-        //Destroy(endObj);
+        bounds = transform.parent.gameObject.GetComponent<RectTransform>() as RectTransform;
+        float left = bounds.transform.position.x - bounds.rect.width / 2;
+        float top = bounds.transform.position.y - bounds.rect.height / 2;
+        float right = bounds.transform.position.x + bounds.rect.width / 2;
+        float bottom = bounds.transform.position.y + bounds.rect.height / 2; 
+        if(isHorizontal)
+        {
+            start.x = left + platformSize.x / 2; 
+            end.x = right - platformSize.x / 2;
+            start.y = end.y = transform.position.y;
+        }  
+        else 
+        {
+            start.y = bottom - platformSize.y / 2; 
+            end.y = top + platformSize.y / 2;
+            start.x = end.x = transform.position.x;
+        }  
+        transform.position = start;
     }
+
     private void FixedUpdate()
     {
-        start = startObj.transform.position;
-        end = endObj.transform.position;
         now = transform.position;
         if (direction)
         {
             if (Vector2.Distance(now, end) <= speed * Time.fixedDeltaTime)
                 direction = false;
             move = (end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.fixedDeltaTime, 0f, Vector2.Distance(now, end));
-            //rigidBody.MovePosition(Vector2.Lerp(start, end, interpolate));
-            //transform.position = now + (end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.fixedDeltaTime, 0f, Vector2.Distance(now, end));
         }
         else
         {
             if (Vector2.Distance(now, start) <= speed * Time.fixedDeltaTime)
                 direction = true;
             move = -(end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.fixedDeltaTime, 0f, Vector2.Distance(now, start));
-            //rigidBody.MovePosition(Vector2.Lerp(end, start, interpolate));
-            //transform.position = now - (end - start) / Vector2.Distance(start, end) * Mathf.Clamp(speed * Time.fixedDeltaTime, 0f, Vector2.Distance(now, start));
         }
         rigidBody.MovePosition(now + move);
         passengers.ForEach(Move);
     }
+
     private void Move(Rigidbody2D passenger)
     {
-        passenger.transform.position=(Vector2)passenger.transform.position + move;
+        passenger.MovePosition((Vector2)passenger.transform.position + move);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         passengers.Add(collision.gameObject.GetComponent<Rigidbody2D>());
-        //collision.gameObject.transform.parent = transform;
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
         passengers.Remove(collision.gameObject.GetComponent<Rigidbody2D>());
-        //collision.gameObject.transform.parent = null;
     }
 }
