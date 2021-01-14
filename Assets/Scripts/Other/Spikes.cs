f878e3185ee903c0e179fa3859f7f6f73e2cb72a
edjack <azarovdima2000@gmail.com>
2021-01-15 01:19:34 +0300

    fix thorns and spikes
    now spikes and thorns are different scripts and created thorns prefab

--- Assets/Scripts/Other/Spikes.cs
+++ Assets/Scripts/Other/Spikes.cs
@@ -5,28 +5,10 @@ using UnityEngine;
 public class Spikes : MonoBehaviour
 {
     public float damageValue;
-    public bool isUp=true;
-    public Animator controllerIsUp;
-
-    private void Start()
-    {
-        StartCoroutine(ChangeState());
-    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (isUp)
-        {
-            col.gameObject.GetComponent<HealthManager>().Damage(damageValue);
-            col.gameObject.GetComponent<MovementManager>().DamagedPush(transform.position);
-        }
-
-    }
-    IEnumerator ChangeState()
-    {
-        yield return new WaitForSeconds(3f);
-        isUp = !isUp;
-        controllerIsUp.SetBool("isUp", isUp);
-        StartCoroutine(ChangeState());
+        col.gameObject.GetComponent<HealthManager>().Damage(damageValue);
+        col.gameObject.GetComponent<MovementManager>().DamagedPush(transform.position);
     }
 }
---
+++ Assets/Scripts/Other/Thorns.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thorns : MonoBehaviour
+{
+    public float damageValue;
+    public Animator controllerIsUp;
+    private bool isUp = true;
+    private List<Collider2D> targets;
+    void Start()
+    {
+        StartCoroutine(ChangeState());
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (isUp)
+        {
+            Damage(col);
+        }
+        else
+            targets.Add(col);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        targets.Remove(collision);
+    }
+    IEnumerator ChangeState()
+    {
+        yield return new WaitForSeconds(3f);
+        controllerIsUp.SetBool("isUp", !isUp);
+        yield return new WaitForSeconds(0.2f);
+        isUp = !isUp;
+        if (isUp)
+        {
+            targets.ForEach(Damage);
+        }
+        StartCoroutine(ChangeState());
+    }
+    private void Damage(Collider2D col)
+    {
+        col.gameObject.GetComponent<HealthManager>().Damage(damageValue);
+        col.gameObject.GetComponent<MovementManager>().DamagedPush(transform.position);
+    }
+}
