24f82d5ad13abe33c174551e81454785a97a9260
Mikhail Oleynik <oleinik.ms@phystech.edu>
2021-01-21 01:58:32 +0300

    feat level_border for camera

--- Assets/Scripts/Other/CameraFollow2D.cs
+++ Assets/Scripts/Other/CameraFollow2D.cs
@@ -3,16 +3,37 @@ using System.Collections;
 
 public class CameraFollow2D : MonoBehaviour {
 
+	public RectTransform _rect;
 	public float damping = 1.5f;
 	public Vector2 offset = new Vector2(2f, 1f);
 	public bool faceLeft;
 	private Transform player;
 	private int lastX;
+	private float xMin;
+	private float yMin;
+	private float xMax;
+	private float yMax;
 
 	void Start ()
 	{
+		CalcBorders();
 		offset = new Vector2(Mathf.Abs(offset.x), offset.y);
 		FindPlayer(faceLeft);
+	}
+
+	void CalcBorders()
+	{
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0,0)); // bottom-left corner
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1,1)); // top-right corner
+		float height = (max - min).y;
+		float width = (max - min).x;
+
+		Vector3[] v = new Vector3[4];
+		_rect.GetWorldCorners(v);
+		xMin = v[0].x + width / 2;
+		yMin = v[0].y + height / 2;
+		xMax = v[2].x - width / 2;
+		yMax = v[2].y - height / 2;
 	}
 
 	public void FindPlayer(bool playerFaceLeft)
@@ -47,6 +68,10 @@ public class CameraFollow2D : MonoBehaviour {
 				target = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
 			}
 			Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
+
+			currentPosition.x = Mathf.Clamp(currentPosition.x, xMin, xMax);
+			currentPosition.y = Mathf.Clamp(currentPosition.y, yMin, yMax);
+
 			transform.position = currentPosition;
 		}
 	}
