749bd5988e46cf38de4283ac43bc2dfa3b951ed8
Mikhail Oleynik <oleinik.ms@phystech.edu>
2021-01-27 22:53:30 +0300

    feat firstCheckpoint for fixing bug, delete spawnPoint

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public GameData gameData;
     public GameObject player;
-    public GameObject spawnPoint;
+    public GameObject firstCheckpoint;
 
     void Awake()
     {
@@ -15,8 +15,8 @@ public class GameManager : MonoBehaviour
 
     void CreatePlayer()
     {
+        AddFirstCheckpoint();
         player = Instantiate(gameData.playerData.prefab, gameData.levelData.checkpoints[0].transform.position, Quaternion.identity, transform);
-        gameData.playerData.spawnPoint = spawnPoint;
         gameData.playerData.animator = player.GetComponent<Animator>();
         gameData.playerData.rigidBody = player.GetComponent<Rigidbody2D>();
         gameData.playerData.boxCollider = player.GetComponent<BoxCollider2D>();
@@ -27,11 +27,14 @@ public class GameManager : MonoBehaviour
         CleanCheckpoints();
     }
 
+    void AddFirstCheckpoint()
+    {
+        gameData.levelData.checkpoints.Add(firstCheckpoint);
+    }
+
     void CleanCheckpoints()
     {
-        GameObject firstCheckpoint = gameData.levelData.checkpoints[0];
-        for (int i = gameData.levelData.checkpoints.Count - 1; i > 0; i--){
-            gameData.levelData.checkpoints.RemoveAt(i);
-        }
+        //GameObject firstCheckpoint = gameData.levelData.checkpoints[0];
+        gameData.levelData.checkpoints.Clear();
     }
 }
